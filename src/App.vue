8da0fc68705fd3e158424a938d20a4a8df22efa7
kritoma <toma.kristian97@gmail.com>
2022-06-01 16:59:52 +0200

    aggiunto footer top

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <BaseHeader/>
     <BaseMain/>
+    <FooterTop/>
   </div>
 </template>
 
 <script>
 import BaseHeader from './components/BaseHeader.vue';
 import BaseMain from './components/BaseMain.vue';
+import FooterTop from './components/footer/FooterTop.vue';
 
 export default {
   name: 'App',
   components: {
     BaseHeader,
     BaseMain,
+    FooterTop,
   }
 }
 </script>
---
+++ src/components/footer/FooterTop.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="footer_top">
+         <div class="container">
+            <ul>
+                <li>
+                    <img src="../../assets/img/buy-comics-merchandise.png" alt="merchandise">
+                    <span>DC MERCHANDISE</span>
+                </li>
+                 <li>
+                    <img src="../../assets/img/buy-comics-subscriptions.png" alt="merchandise">
+                    <span>SUBSCRIPTION</span>
+                </li>
+                 <li>
+                    <img src="../../assets/img/buy-comics-shop-locator.png" alt="merchandise">
+                    <span>COMIC SHOP LOCATOR</span>
+                </li>
+                 <li>
+                    <img src="../../assets/img/buy-dc-power-visa.svg" alt="merchandise">
+                    <span>DC POWER VISA</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FooterTop",
+}
+</script>
+
+<style lang="scss" scoped>
+.footer_top {
+    background-color: var(--color-primary);
+    padding: 1.875rem 0;
+
+    ul {
+        display: flex;
+        list-style: none;
+        justify-content: center;
+        gap: 1.875rem;
+
+        li {
+            display: flex;
+            align-items: center;
+
+            img {
+                height: 3rem;
+                margin-right: .9375rem;
+            }
+
+            span {
+                color: white;
+            }
+        }
+    }
+}
+</style>
