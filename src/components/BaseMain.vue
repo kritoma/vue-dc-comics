be8f16784e36ead8cc3a7529e2a7749176bd8d19
kritoma <toma.kristian97@gmail.com>
2022-06-01 16:38:04 +0200

    aggiunto corpo main

---
+++ src/components/BaseMain.vue
@@ -0,0 +1,25 @@
+<template>
+<main>
+    <div class="container">
+        <span>--> Content goes here</span> 
+    </div>
+</main>
+</template>
+
+<script>
+export default {
+    name: "BaseMain",
+}
+</script>
+
+<style lang="scss" scoped>  
+main {
+    background-color: #1c1c1c;
+    min-height: 6.25rem;
+
+    span {
+        color: white;
+        line-height: 6.25rem;
+    }
+}
+</style>
