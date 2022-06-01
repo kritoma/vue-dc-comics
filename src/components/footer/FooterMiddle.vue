2d2030639183501a25a5f6aced5ff9bee4819f64
kritoma <toma.kristian97@gmail.com>
2022-06-01 19:39:47 +0200

    lavorazione footer middle

---
+++ src/components/footer/FooterMiddle.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="footer_middle">
+      <div class="container">
+          <div class="list-footer">
+                <div class="column">
+
+                </div>
+                <div class="column">
+                    <h3>DC</h3>
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choicces</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="column">
+
+                </div>
+          </div>
+          <div class="logo_footer">
+              <img src="../../assets/img/dc.logo-bg.png" alt="logo">
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "FooterMiddle",
+}
+</script>
+
+<style lang="scss" scoped>
+.footer_middle {
+    background-image: url('../../assets/img/footer-bg.jpg');
+}
+</style>
